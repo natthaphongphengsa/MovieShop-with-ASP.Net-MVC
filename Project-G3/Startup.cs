739ce686ebab16d35772252971e73f88a08b63e2
SimonKva <80454747+SimonKva@users.noreply.github.com>
2021-04-12 11:53:34 +0200

    Add project files.

---
+++ Project-G3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_G3.Startup))]
+namespace Project_G3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
