26e5c3332de0bb2fff2c1da471d677b3329e0920
Andreas87K <andreasgunnarkarlsson@gmail.com>
2021-05-20 15:46:07 +0200

    FlashSale is up and working /Andreas

--- Project-G3/Controllers/HomeController.cs
+++ Project-G3/Controllers/HomeController.cs
@@ -8,37 +8,37 @@ using System.Web.Mvc;
 namespace Project_G3.Controllers
 {
     public class HomeController : Controller
-    {        
+    {
         private ApplicationDbContext db = new ApplicationDbContext();
         public HomeController()
         {
             ViewData["Genres"] = db.Genres.ToList();
         }
         public ActionResult Index()
-        {            
+        {
             List<Movie> CartList = HttpContext.Session["ShoppingCart"] != null ? (List<Movie>)HttpContext.Session["ShoppingCart"] : new List<Movie>();
             //Visa Antal film som finns i varukorg i index sidan
-            int amount = 0;    
+            int amount = 0;
             //Om varukorg är tomt sätt 0
-            if (CartList.Count != 0) {  amount = CartList.Count; } else { amount = 0; }
+            if (CartList.Count != 0) { amount = CartList.Count; } else { amount = 0; }
             ViewBag.Amount = amount;
             return View(db.Movies.ToArray());
         }
         public ActionResult Cart()
         {
             List<Movie> CartList = HttpContext.Session["ShoppingCart"] != null ? (List<Movie>)HttpContext.Session["ShoppingCart"] : new List<Movie>();
-            ViewData["ShoppingCart"] = CartList;            
+            ViewData["ShoppingCart"] = CartList;
             decimal TotalPrice = 0;
             foreach (var item in CartList) { TotalPrice += item.MoviePrice; }
             ViewBag.Sum = TotalPrice;
-            
+
             return View(CartList);
         }
-        
+
         public ActionResult AddToCart(int Id)
         {
             //ApplicationDbContext _db = new ApplicationDbContext();
-            List<Movie> CartList = HttpContext.Session["ShoppingCart"]!=null ? (List<Movie>) HttpContext.Session["ShoppingCart"] : new List<Movie>();
+            List<Movie> CartList = HttpContext.Session["ShoppingCart"] != null ? (List<Movie>)HttpContext.Session["ShoppingCart"] : new List<Movie>();
             Movie movie = db.Movies.First(m => m.MovieId == Id);
             if (!CartList.Any(m => m.MovieId == movie.MovieId)) CartList.Add(movie);
             HttpContext.Session["ShoppingCart"] = CartList;
@@ -53,7 +53,7 @@ namespace Project_G3.Controllers
         }
         public ActionResult About()
         {
-            
+
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -74,7 +74,7 @@ namespace Project_G3.Controllers
             //ApplicationDbContext db = new ApplicationDbContext();
             Movie movie = db.Movies.FirstOrDefault(m => m.MovieId == id);
             return View(movie);
-        }  
+        }
         public ActionResult PaymentMethod()
         {
             return View();
@@ -83,5 +83,49 @@ namespace Project_G3.Controllers
         {
             return View();
         }
+
+        public ActionResult GetFlashSale()
+        {
+            List<Movie> movies = new List<Movie>();
+            List<FlashSale> flashsales = db.FlashSales.ToList();
+            foreach (FlashSale item in flashsales)
+            {
+
+                bool isprocentedbased = item.FlashSaleDiscount[item.FlashSaleDiscount.Length - 1] == '%';
+
+                foreach (Movie movie in item.Movies)
+                {
+                    decimal discount;
+                    if (isprocentedbased)
+                    {
+                        discount = Decimal.Parse(item.FlashSaleDiscount.Substring(0, item.FlashSaleDiscount.Length - 1)) / 100;
+                        movie.MoviePrice = movie.MoviePrice - movie.MoviePrice * discount;
+
+                    }
+                    else
+                    {
+                        discount = decimal.Parse(item.FlashSaleDiscount);
+                        movie.MoviePrice = movie.MoviePrice - discount;
+                    }
+
+                    Movie Mo = movies.Find(m => m.MovieId == movie.MovieId);
+                    if (Mo == null)
+                    {
+                        movies.Add(movie);
+
+                    }
+                    else if (Mo.MoviePrice > movie.MoviePrice)
+                    {
+                        movies.Remove(Mo);
+                        movies.Add(movie);
+                    }
+
+                    
+                }
+
+            }
+            return View(movies);
+        }
+
     }
 }
