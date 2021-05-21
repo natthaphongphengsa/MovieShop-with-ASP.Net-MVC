e73e806d62965b313b981f96861310dfd9429efd
Irina <irina.gaydukova07@gmail.com>
2021-05-21 09:42:41 +0200

    Add genre sortering

--- Project-G3/Controllers/HomeController.cs
+++ Project-G3/Controllers/HomeController.cs
@@ -73,9 +73,12 @@ namespace Project_G3.Controllers
             Movie movie = db.Movies.FirstOrDefault(m => m.MovieId == id);
             return View(movie);
         }  
-        public ActionResult Genre()
+        public ActionResult Genre(int Id)
         {
-            return View(db.Genres.ToList());
+            List<Movie> movies = db.Genres.First(g => g.GenreId == Id).Movies.ToList();
+            return View(movies);
         }
+
+
     }
 }
