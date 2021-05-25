2e8169891ec1ca19ccf90a2f66308eb50489fd7b
Andreas87K <andreasgunnarkarlsson@gmail.com>
2021-05-25 11:41:11 +0200

    FlashSale Update

---
+++ Project-G3/Models/MovieViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_G3.Models
+{
+    public class FlashSalePriceViewModel
+    {
+       public Movie Movie { get; set; }
+        public string NewPrice { get; set; }
+
+        public string FlashSale { get; set; }
+
+
+    }
+}
