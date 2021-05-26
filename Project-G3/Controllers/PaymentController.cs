2791a8f16229c6067437b873407014801e0b2925
Natthaphong Phengsa <natthaphong.phengsa.syne20lin@tucsweden.se>
2021-05-26 23:51:13 +0200

    Fix Cart function/and style

--- Project-G3/Controllers/PaymentController.cs
+++ Project-G3/Controllers/PaymentController.cs
@@ -10,15 +10,34 @@ namespace Project_G3.Controllers
 {
     public class PaymentController : Controller
     {
-        // GET: Payment
-        public ActionResult PaymentMethod()
+        // GET: Payment        
+        public ActionResult PaymentMethod(FormDetails CustomInfo, int Id)
         {
-            return View();
+            if (Session["Email"] == null && Id == 2)
+            {
+                return RedirectToAction("Login","Account");
+            }
+            else if(Session["Email"] != null && Id == 2)
+            {
+                return View(CustomInfo);
+            }
+            else if(Session["Email"] != null && Id == 1)
+            {
+                return View(CustomInfo);
+            }
+            else
+            {
+                return View(CustomInfo);
+            }
+            
         }
-        public ActionResult Receipt()
+        public ActionResult Receipt(FormDetails FD)
         {
             List<Movie> CartList = HttpContext.Session["ShoppingCart"] != null ? (List<Movie>)HttpContext.Session["ShoppingCart"] : new List<Movie>();
-            ViewData["ShoppingCart"] = CartList;
+            //ViewData["ShoppingCart"] = CartList;
+            List<FormDetails> info = new List<FormDetails>();
+            info.Add(FD);
+            ViewData["CustomDetails"] = info;
             decimal TotalPrice = 0;
             foreach (var item in CartList) { TotalPrice += item.MoviePrice; }
             ViewBag.Sum = TotalPrice;
